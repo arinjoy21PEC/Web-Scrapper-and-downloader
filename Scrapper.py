210d6a79bb417a78bd7d6e203add29a3b0df8279
Arinjoy Nandy <arinjoynandy2019@gmail.com>
2023-07-05 03:13:45 +0530

    update

--- Scrapper.py
+++ Scrapper.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 from tkinter import ttk, filedialog
 import requests
-import os
+from bs4 import BeautifulSoup
+
 class Downloader:
     def __init__(self):
         self.saveto = ""
@@ -11,10 +12,12 @@ class Downloader:
         self.url_label.pack()
         self.url_entry = tk.Entry()
         self.url_entry.pack()
-        self.browse_button = tk.Button(text='Browser', command=self.browse_file)
+        self.browse_button = tk.Button(text='Browse', command=self.browse_file)
         self.browse_button.pack()
         self.download_button = tk.Button(text='Download', command=self.download)
         self.download_button.pack()
+        self.modify_button = tk.Button(text='Modify', command=self.modify)
+        self.modify_button.pack()
         self.window.geometry("844x344")
         self.progress_bar = ttk.Progressbar(self.window, orient='horizontal', maximum=100, length=300, mode='determinate')
         self.progress_bar.pack()
@@ -22,23 +25,41 @@ class Downloader:
 
     def browse_file(self):
         saveto = filedialog.asksaveasfilename(initialfile=self.url_entry.get().split("/")[-1]).split("?")[0]
+        if not saveto.endswith(".html"):
+            saveto += ".html"
         self.saveto = saveto
 
     def download(self):
         url = self.url_entry.get()
         response = requests.get(url, stream=True)
         total_size_in_bytes = 10000
-        if (response.headers.get("content-length")):
+        if response.headers.get("content-length"):
             total_size_in_bytes = int(response.headers.get('Content-Length', 0))
         block_size = 10000
         self.progress_bar['value'] = 0
         filename = self.url_entry.get().split("/")[-1].split("?")[0]
-        if(self.saveto == ""):
+        if not filename.endswith(".html"):
+            filename += ".html"
+        if self.saveto == "":
             self.saveto = filename
         with open(self.saveto, 'wb') as f:
             for data in response.iter_content(block_size):
                 self.progress_bar['value'] += (100 * block_size) / total_size_in_bytes
                 self.window.update()
                 f.write(data)
+
+        print(f"File downloaded to {self.saveto}")
+
+    def modify(self):
+        if self.saveto.endswith(".html") or self.saveto.endswith(".htm"):
+            with open(self.saveto, "r", encoding="utf-8") as html_file:
+                soup = BeautifulSoup(html_file, "html.parser")
+                text_content = soup.get_text()
+                text_filename = self.saveto.rsplit(".", 1)[0] + ".txt"
+                with open(text_filename, "w", encoding="utf-8") as text_file:
+                    text_file.write(text_content)
+                print(f"Text content downloaded to {text_filename}")
+        else:
+            print("The file is not an HTML file.")
 
 Downloader()
